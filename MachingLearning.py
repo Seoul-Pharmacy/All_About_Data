fd601a9e7a324f5218267ac4833a8e1edddf7514
soohyunkyung <113413847+soohyunkyung@users.noreply.github.com>
2024-04-16 18:52:27 +0900

    Update MachingLearning.py

--- MachingLearning.py
+++ MachingLearning.py
@@ -1,14 +1,20 @@
 from sklearn.neighbors import KNeighborsClassifier
+from haversine import haversine
 
-def get_pharmacy(longitude, latitude)
+def get_pharmacy(longitude, latitude, user_location)
   kn=KNeighborsClassifier()
   kn.fit(longitude, latitude) #드롭다운 된 경도, 위도를 가져와야 합니다.
-  user=[경도, 위도] #사용자의 경도, 위도입니다.
-  kn.predict([new]) 
-  distances, indexes=kn.neighbors([new])
-
+  kn.predict([user_location]) 
+  distances, indexes=kn.neighbors([user_location])
+  
+  name_results=[]
   for i in indexes:
-    print(name[i], gu[i]+" "+road_name_adress[i]) #사용자 주변 5개의 약국 이름, 약국 주소를 출력합니다.
+    name_results.append(name[i])
+    
+  address_results=[] 
+  for i in indexes:
+    address_results.append(gu[i]+" "+road_name_adress[i]) #사용자 주변 5개의 약국 이름, 약국 주소를 출력합니다.
 
-  for i in len(indexes):
-    print(distances[i]) #거리 변환이 필요합니다.
+  distances_results=[]
+  for i in indexes:
+    distances_results.append(haversine(user_location,(longitude[i],latitude[i]))+"km")
