f6d531496c484c18ac85af0e4c934ba38b378465
soohyunkyung <113413847+soohyunkyung@users.noreply.github.com>
2024-04-17 14:49:09 +0900

    Update MachingLearning.py

--- MachingLearning.py
+++ MachingLearning.py
@@ -4,9 +4,8 @@ from haversine import haversine
 def get_pharmacy(datas, user_latitude, user_longitude):
     filtered_longitude=[]
     filtered_latitude=[]
-    name_results=[]
-    address_results=[] 
     distances_results=[]
+    datas_results=[]
     for i in range(len(datas)):
         filtered_longitude.append(float(datas[i]['longitude']))
         filtered_latitude.append(float(datas[i]['latitude']))
@@ -19,12 +18,9 @@ def get_pharmacy(datas, user_latitude, user_longitude):
     distances, indexes=kn.kneighbors([user_latitude])
   
     for i in indexes[0]:
-        name_results.append(datas[i]['name']) #사용자 주변 5개의 약국 이름을 저장합니다.
+        datas_results.append(datas[i])
     
     for i in indexes[0]:
-        address_results.append(datas[i]['gu']+" "+datas[i]['road_name_address']) #사용자 주변 5개의 약국 주소를 저장합니다.
+        distances_results.append(str(haversine((float(user_latitude[0]), float(user_longitude[0])),(float(datas[i]['latitude']),float(datas[i]['longitude']))))+"km") #사용자로부터 가까운 5개 약국과의 거리를 저장합니다.
 
-    for i in indexes[0]:
-        distances_results.append(haversine((float(user_latitude[0]), float(user_longitude[0])),(float(datas[i]['latitude']),float(datas[i]['longitude'])))) #사용자로부터 가까운 5개 약국과의 거리를 저장합니다.
-
-    return name_results, address_results, distances_results
+    return datas_results, distances_results
